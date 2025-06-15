211b6d706f49c305d3f5586645e5c1a05acc88f9
Bulat Khafizov <bnkhafizov@kpfu.ru>
2025-06-15 16:37:09 +0300

    Delay Liquibase property resolution to task execution phase and fix run-local.sh
    - Replaced direct project.property(...) calls with findProperty(...) to avoid build failure
      when properties are missing

--- run-local.sh
+++ run-local.sh
@@ -1,4 +1,4 @@
-docker compose -f docker/docker-compose.local.yml up -d
+docker compose -f docker/docker-compose.local.yml -p eat-easy up -d
 docker compose -f services/gateway-service/docker/docker-compose.local.yml -p eat-easy up -d
 docker compose -f services/authentication-service/docker/docker-compose.local.yml -p eat-easy up -d
 docker compose -f services/user-service/docker/docker-compose.local.yml -p eat-easy up -d
